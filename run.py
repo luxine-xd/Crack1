1688b6453309972d6087b9df5e733c27a68793bf
luxine-xd <121477745+luxine-xd@users.noreply.github.com>
2023-04-03 14:58:48 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,13 @@
+import platform
+import os
+try:os.mkdir('/sdcard/OK')
+except:pass
+try:os.mkdir('/sdcard/CP')
+except:pass
+arc = str(platform.uname().machine)
+if 'arm' in arc:
+	__import__("latter")._site_view_()
+elif 'aarch' in arc:
+	__import__("lunixe").cek_login()
+else:
+	exit(f' Unknow device machine {arc}')
